7b0fab633879fc76f3e7e7c0d6e3391db43ce538
Zaleth Mata <desarrolladord3@PASSWORDCONSULTING.onmicrosoft.com>
2025-10-19 10:00:24 -0500

    fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAdNIwZXDY9Wi0Evfkg431OAiGFAciBzD0',
+    authDomain: 'pragma-test-7efa2.firebaseapp.com',
+    projectId: 'pragma-test-7efa2',
+    storageBucket: 'pragma-test-7efa2.firebasestorage.app',
+    messagingSenderId: '792465550483',
+    appId: '1:792465550483:web:d6af8b1bd3c0e4237d7166',
+    measurementId: 'G-2B5GSN1M7R',
+  },
 };
